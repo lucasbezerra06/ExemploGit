315e6333dd361b40941396d4737d9488a85efe73
Lucas <lucasbezerra06@gmail.com>
2017-08-16 21:56:39 -0300

    Primeiro commit

---
+++ src/controller/SoController.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class SoController {
+	public SoController() {
+		super();
+	}
+	
+	public String os(){
+		return System.getProperty("os.name");
+	}
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,11 @@
+package view;
+
+import controller.SoController;
+
+public class Main {
+	public static void main(String args[]){
+		SoController soController = new SoController();
+		System.out.println(soController.os());
+	}
+
+}
